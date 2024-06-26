2e79bbed0f8037c2423902320d6db91e7c25e3a5
LAPTOP-K6IS3HAR\khsal <khsaleem@hotmail.com>
2024-06-26 12:39:10 -0500

    Initial Release

---
+++ StudentApp/StudentApp/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentApp.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+    }
+}
